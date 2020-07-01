a46ff3a0a10c7fdec765069805f74c5eb0fda42f
paulracooni <paulracooni@gmail.com>
2020-07-01 21:31:56 +0900

    feat: Change authentication rule
    <description>
    - If no Id Token -> Anonymous user
    - if Anonymous user -> Anonymous user
    - If not registered user ->  Anonymous user
    - if registered user -> User

--- wilt_api/firebase_authentication/authentication.py
+++ wilt_api/firebase_authentication/authentication.py
@@ -3,7 +3,9 @@ __all__ = "FirebaseAuthentication", "FirebaseAuthMiddleware", "firebase_app"
 from django.conf import settings
 from django.contrib.auth import get_user_model, middleware
 
+
 import firebase_admin
+import firebase_authentication
 from django.utils.functional import SimpleLazyObject
 from firebase_admin import auth
 from rest_framework import authentication, exceptions
@@ -14,25 +16,22 @@ UserModel = get_user_model()
 credentials = firebase_admin.credentials.Certificate(settings.FIREBASE_PATH)
 firebase_app = firebase_admin.initialize_app(credentials)
 
-
 def verify_user_token(token):
+    
     try:
         return auth.verify_id_token(token)
     except Exception:
-        raise exceptions.InvalidAuthToken()
+        # raise exceptions.InvalidAuthToken()
+        return None
+        
+def get_user_or_anonymous(user_data):
 
-def get_or_create_user(user_data):
+    try:
+        user = UserModel.objects.get(id=user_data.get('uid'))
+    except Exception :
+        user = get_anonymous()
 
-    user, created = UserModel.objects.get_or_create(
-        id=user_data.get('uid'),
-        defaults={"email": user_data.get('email')}
-    )
-    
     return user
-
-def is_anonymous(user_data):
-    
-    return user_data['firebase']['sign_in_provider'] == 'anonymous'
 
 def get_anonymous():
 
@@ -42,8 +41,9 @@ def get_anonymous():
     )
     return user
 
-
-
+def is_anonymous(user_data):
+    
+    return user_data['firebase']['sign_in_provider'] == 'anonymous'
 
 class AuthenticationMixin:
 
@@ -55,18 +55,19 @@ class AuthenticationMixin:
 
         token = self.get_auth_token(request)
         user_data = verify_user_token(token)
-
-        return self.ck_anony_and_get_user(user_data)
-
-    @staticmethod
-    def ck_anony_and_get_user(user_data):
-
-        if is_anonymous(user_data):
-            user = get_anonymous()
-        else:
-            user = get_or_create_user(user_data)
         
-        return user
+        return get_user_or_anonymous(user_data)
+
+    # @staticmethod
+    # def ck_anonymous_and_get_user(user_data):
+
+    #     if is_anonymous(user_data):
+    #         user = get_user(user_data)
+    #         # user = get_anonymous()
+    #     else:
+    #         user = get_user(user_data)
+        
+    #     return user
 class FirebaseAuthentication(AuthenticationMixin, authentication.BaseAuthentication):
 
     @staticmethod
@@ -81,7 +82,6 @@ class FirebaseAuthentication(AuthenticationMixin, authentication.BaseAuthenticat
         user = super(FirebaseAuthentication, self).authenticate(request)
 
         return user, None
-
 
 class FirebaseAuthMiddleware(AuthenticationMixin, middleware.AuthenticationMiddleware):
 
