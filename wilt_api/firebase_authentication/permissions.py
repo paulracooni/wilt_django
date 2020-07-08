0885e73d9025819dd1c5e1c89c212dfb07e3cd0b
paulracooni <paulracooni@gmail.com>
2020-07-08 16:56:26 +0900

    feat: Add new permission
    <description>
    - IsAuthenticatedOrReadonly
    - IsAuthorUpdateOrReadOnly

--- wilt_api/firebase_authentication/permissions.py
+++ wilt_api/firebase_authentication/permissions.py
@@ -1,52 +1,61 @@
-from rest_framework.permissions import SAFE_METHODS
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
-class IsAuthenticated(BasePermission):
+class IsAuthenticated(permissions.BasePermission):
     def has_permission(self, request, view):
-        print("In IsAuthenticated.has_permission")
-        print(request.user)
         return bool(request.user and request.user.is_authenticated)
 
 
-class IsAdminUser(BasePermission):
+class IsAuthenticatedOrReadonly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(slef, request, views, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return False
+
+
+class IsAdminUser(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_staff)
 
 
-class IsMyself(BasePermission):
+class IsMyself(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated
 
     def has_object_permission(slef, request, views, obj):
 
-        if request.method in SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
 
         return obj.id == request.user.id
 
 
-class IsAuthorOrReadonly(BasePermission):
+class IsAuthorOrReadonly(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated
 
     def has_object_permission(slef, request, views, obj):
 
-        if request.method in SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.author == request.user
+        return obj.user == request.user
 
 
-class IsAuthorUpdateOrReadOnly(BasePermission):
+class IsAuthorUpdateOrReadOnly(permissions.BasePermission):
     def has_permission(slef, request, view):
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
 
         if request.method == "DELETE":
             return request.user.is_superuser
 
-        return obj.author == request.user
+        return obj.user == request.user
