99387134b1efc5da0daa80ca75fbb3dd1735778f
paulracooni <paulracooni@gmail.com>
2020-07-08 14:54:51 +0900

    feat: Impl TilSerializer

--- wilt_api/wilt_til/serializers.py
+++ wilt_api/wilt_til/serializers.py
@@ -9,6 +9,7 @@ class TilSerializer(serializers.ModelSerializer):
     class Meta:
         model = Til
         fields = "__all__"
+        read_only_fields = ["id", "author", "date_created"]
 
     # def create(self, validated_data):
     #     return User.objects.create_user(**validated_data)
