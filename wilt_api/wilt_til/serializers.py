00c178ff2922d45f8fae7cffa4b2626e6e2b9631
paulracooni <paulracooni@gmail.com>
2020-07-08 18:10:51 +0900

    feat: Impl ClapSerializer, BookmarkSerializer

--- wilt_api/wilt_til/serializers.py
+++ wilt_api/wilt_til/serializers.py
@@ -1,12 +1,31 @@
 from rest_framework import serializers
 
-from wilt_til.models import Til
+from wilt_til.models import Til, Clap, Bookmark
 
 __all__ = ("TilSerializer",)
+
+ROF = (
+    "id",
+    "date_created",
+)
 
 
 class TilSerializer(serializers.ModelSerializer):
     class Meta:
         model = Til
         fields = "__all__"
-        read_only_fields = ["id", "date_created"]
+        read_only_fields = ROF
+
+
+class ClapSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clap
+        fields = "__all__"
+        read_only_fields = ROF
+
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookmark
+        fields = "__all__"
+        read_only_fields = ROF
