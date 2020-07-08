b9b463022d773bec5c5b7ca5341fedf6ac2440fc
paulracooni <paulracooni@gmail.com>
2020-07-08 19:55:37 +0900

    fix: Set default value of is_active,  is_public <description> - default value of TilSerializer.is_active is "True" - default value of TilSerializer.is_public is "True"

--- wilt_api/wilt_til/serializers.py
+++ wilt_api/wilt_til/serializers.py
@@ -4,28 +4,31 @@ from wilt_til.models import Til, Clap, Bookmark
 
 __all__ = ("TilSerializer",)
 
-ROF = (
+GLOBAL_ROF = (
     "id",
     "date_created",
 )
 
 
 class TilSerializer(serializers.ModelSerializer):
+    is_active = serializers.BooleanField(default=True)
+    is_public = serializers.BooleanField(default=True)
+
     class Meta:
         model = Til
         fields = "__all__"
-        read_only_fields = ROF
+        read_only_fields = GLOBAL_ROF
 
 
 class ClapSerializer(serializers.ModelSerializer):
     class Meta:
         model = Clap
         fields = "__all__"
-        read_only_fields = ROF
+        read_only_fields = GLOBAL_ROF
 
 
 class BookmarkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bookmark
         fields = "__all__"
-        read_only_fields = ROF
+        read_only_fields = GLOBAL_ROF
