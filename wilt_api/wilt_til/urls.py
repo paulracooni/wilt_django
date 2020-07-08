07831e7ccf5e79d7d7eee955ebbb2804aeb8462d
paulracooni <paulracooni@gmail.com>
2020-07-08 21:05:47 +0900

    feat: Impl TilBookmark view

--- wilt_api/wilt_til/urls.py
+++ wilt_api/wilt_til/urls.py
@@ -6,6 +6,7 @@ from wilt_til import views
 urlpatterns = [
     path("", views.TilListCreate.as_view()),
     path("<int:id>/", views.TilRetrieveUpdateDestroy.as_view()),
+    path("<int:id>/bookmark", views.TilBookmark.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- wilt_api/wilt_til/views.py
+++ wilt_api/wilt_til/views.py
@@ -2,22 +2,31 @@ from django.http import Http404
 from django.db.models import Q
 
 from rest_framework import status
+from rest_framework import mixins
 from rest_framework import filters
 from rest_framework import generics
 from rest_framework import pagination
+
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
+from rest_framework.decorators import api_view
 
 from firebase_authentication import exceptions
 from firebase_authentication import permissions
 
 from wilt_til.models import Til, Clap, Bookmark
 from wilt_til.serializers import TilSerializer
+from wilt_til.serializers import ClapSerializer
+from wilt_til.serializers import BookmarkSerializer
 
 from firebase_admin import auth
 
-__all__ = ("TilListCreate", "TilRetrieveUpdateDestroy")
+__all__ = (
+    "TilListCreate",
+    "TilRetrieveUpdateDestroy",
+    "TilBookmark",
+)
 
 # ////////////////////////////////////////////
 # Define filters
@@ -80,3 +89,39 @@ class TilRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
         )
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
+
+
+class TilBookmark(generics.GenericAPIView):
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
+    permission_classes = [permissions.IsMyself]
+
+    def post(self, request, id, format=None):
+        data = self.create(til=id, user=request.user.id)
+        headers = self.get_success_headers(data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def delete(self, request, id, format=None):
+        bookmark = self.get_bookmark_or_404(til=id, user=request.user.id)
+        bookmark.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def create(self, til, user):
+        serializer = self.get_serializer(data=dict(til=til, user=user))
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return serializer.data
+
+    def get_bookmark_or_404(self, til, user):
+        try:
+            bookmark = self.get_queryset().get(til=til, user=user)
+        except Bookmark.DoesNotExist as ex:
+            raise Http404
+        return bookmark
+
+    @staticmethod
+    def get_success_headers(data):
+        try:
+            return {"Location": str(data[api_settings.URL_FIELD_NAME])}
+        except (TypeError, KeyError):
+            return {}
