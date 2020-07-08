c940a54727997fd266b78d70829f4ec8ad529605
paulracooni <paulracooni@gmail.com>
2020-07-08 16:53:44 +0900

    fix: Update til model update
    <description>
    - Change filed name fro author to user

--- wilt_api/wilt_til/serializers.py
+++ wilt_api/wilt_til/serializers.py
@@ -9,7 +9,7 @@ class TilSerializer(serializers.ModelSerializer):
     class Meta:
         model = Til
         fields = "__all__"
-        read_only_fields = ["id", "author", "date_created"]
+        read_only_fields = ["id", "user", "date_created"]
 
     # def create(self, validated_data):
     #     return User.objects.create_user(**validated_data)
