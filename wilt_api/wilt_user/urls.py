b818c916c555b6e5650f26c41ee14fbbf798447b
Paul Kim <paulracooni@gmail.com>
2020-07-11 00:55:48 +0900

    fix: wilt_user urls

--- wilt_api/wilt_user/urls.py
+++ wilt_api/wilt_user/urls.py
@@ -6,7 +6,7 @@ from wilt_user import views
 urlpatterns = [
     path("", views.UserCheck.as_view()),
     path("<str:id>/", views.UserDetail.as_view()),
-    path("<str:id>/claps/", views.UserClaps.as_view()),
+    path("<str:id>/claps", views.UserClaps.as_view()),
     path("<str:id>/bookmarks", views.UserBookmark.as_view()),
     path("<str:id>/tags", views.UserTag.as_view()),
 ]
