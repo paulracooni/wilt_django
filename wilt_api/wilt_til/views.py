5c32419598b5d3adc20d04b69dc0a23c3a518804
Paul Kim <paulracooni@gmail.com>
2020-07-11 23:42:01 +0900

    fix: Fix bugs
    <description>
    Bug.1 tags update errror
    -> When tags update, Not parse input string.
    Bug.2 is_active could retrive and update error.
    -> Deleted Til was working.
    All is fixed

--- wilt_api/wilt_til/views.py
+++ wilt_api/wilt_til/views.py
@@ -9,6 +9,7 @@ from rest_framework import pagination
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.settings import api_settings
 
 from firebase_authentication import exceptions
 from firebase_authentication import permissions
@@ -38,6 +39,9 @@ class IsActiveFilterBackend(filters.BaseFilterBackend):
     """
 
     def filter_queryset(self, request, queryset, view):
+        # @TODO: You must delete this condtion if admin page will implementd.
+        if request.method == "PATCH":
+            return queryset
         return queryset.filter(is_active=True)
 
 
@@ -56,7 +60,7 @@ class IsPublicOrMineFilterBackend(filters.BaseFilterBackend):
 
 class IsTilRealtedFilterBackend(filters.BaseFilterBackend):
     """
-    Filter that only lists is_public=True or user's instance
+    Filter that only lists Til related
     """
 
     def filter_queryset(self, request, queryset, view):
@@ -78,43 +82,89 @@ class IdCursorPagination(pagination.CursorPagination):
 # Define views (generics)
 # - [https://www.django-rest-framework.org/api-guide/generic-views/]
 # ////////////////////////////////////////////
-class TilListCreate(generics.ListCreateAPIView):
+def extract_data_with_user_id_form(request):
+    data = dict(request.data.items())
+    data["user"] = request.user.id
+    return data
+
+
+def parse_tag_and_create_if_new(tags):
+
+    if isinstance(tags, str):
+        tags = parse_tag_input(tags)
+    for name in tags:
+        Tag.objects.get_or_create(name=name)
+
+    return tags
+
+
+def get_success_headers(data):
+    try:
+        return {"Location": str(data[api_settings.URL_FIELD_NAME])}
+    except (TypeError, KeyError):
+        return {}
+
+
+class TilListCreate(generics.GenericAPIView):
+    """
+    [GET]
+    - List all api
+    [POST]
+    - Create New Til
+    """
+
     queryset = Til.objects.all()
     serializer_class = TilSerializer
     pagination_class = IdCursorPagination
     permission_classes = [permissions.IsAuthor]
     filter_backends = [IsActiveFilterBackend, IsPublicOrMineFilterBackend]
 
-    def create(self, request, *args, **kwargs):
-        data = self.__put_user_id_data(request)
-        data = self.__parse_tag_and_create_if_new(data)
+    def get(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        data = extract_data_with_user_id_form(request)
+        data["tags"] = parse_tag_and_create_if_new(data.get("tags", ""))
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        headers = self.get_success_headers(serializer.data)
+        headers = get_success_headers(serializer.data)
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
-
-    @staticmethod
-    def __put_user_id_data(request):
-        data = dict(request.data.items())
-        data["user"] = request.user.id
-        return data
-
-    @staticmethod
-    def __parse_tag_and_create_if_new(data):
-        data["tags"] = parse_tag_input(data.get("tags", ""))
-        for name in data["tags"]:
-            tag, created = Tag.objects.get_or_create(name=name)
-        return data
 
 
 class TilRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Til.objects.all()
     serializer_class = TilSerializer
     permission_classes = [permissions.IsAuthor]
+    filter_backends = [IsActiveFilterBackend, IsPublicOrMineFilterBackend]
     lookup_field = "id"
+
+    def update(self, request, *args, **kwargs):
+
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+
+        data = dict(request.data.items())
+        data["tags"] = parse_tag_and_create_if_new(data.get("tags", ""))
+
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
     def perform_destroy(self, instance):
         serializer = self.get_serializer(
