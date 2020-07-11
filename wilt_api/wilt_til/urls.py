22fb0e1ad12d6410553b42c01ce1acefb11c6539
Paul Kim <paulracooni@gmail.com>
2020-07-11 11:54:21 +0900

    Fix: fix urls, append 's'

--- wilt_api/wilt_til/urls.py
+++ wilt_api/wilt_til/urls.py
@@ -6,8 +6,8 @@ from wilt_til import views
 urlpatterns = [
     path("", views.TilListCreate.as_view()),
     path("<int:id>/", views.TilRetrieveUpdateDestroy.as_view()),
-    path("<int:id>/bookmark", views.TilBookmark.as_view()),
-    path("<int:id>/clap", views.TilClap.as_view()),
+    path("<int:id>/bookmarks", views.TilBookmark.as_view()),
+    path("<int:id>/claps", views.TilClap.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
