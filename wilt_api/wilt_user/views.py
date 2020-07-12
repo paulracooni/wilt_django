5faa34fd42184e81ffc420fdf650f5be79fd26e5
Paul Kim <paulracooni@gmail.com>
2020-07-12 20:17:11 +0900

    refator: Update for response messages

--- wilt_api/wilt_user/views.py
+++ wilt_api/wilt_user/views.py
@@ -26,37 +26,24 @@ __all__ = (
     "UserBookmark",
 )
 
-# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = WiltUser.objects.all()
-#     serializer_class = WiltUserSerializer
-#     permission_classes = [permissions.IsMyself]
-#     lookup_field  = "id"
 
-#     def partial_update(self, request, *args, **kwargs):
-#         kwargs['partial'] = True
-#         return self.update(request, *args, **kwargs)
-
-#     def perform_destroy(self, instance):
-#         serializer = self.get_serializer(
-#             instance, data=dict(is_active=False), partial=True)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_update(serializer)
-
-
-def get_user_or_404(id):
+def get_user_or_false(id):
     try:
         user = WiltUser.objects.get(id=id)
     except WiltUser.DoesNotExist as ex:
-        raise Http404
+        return False
     return user
 
 
-def get_active_user_or_404(id):
-    user = get_user_or_404(id)
-    if not user.is_active:
-        raise Http404
+def get_active_user_or_false(id):
+    user = get_user_or_false(id)
+    if user and not user.is_active:
+        return False
     return user
 
+def get_invalid_user_response(id):
+    detail = f"Invalid user id({id})."
+    return Response(dict(detail=detail), status=status.HTTP_404_NOT_FOUND)
 
 class UserDetail(APIView):
 
@@ -64,26 +51,45 @@ class UserDetail(APIView):
     NO_UPDATE_FIELD = ("id", "email", "is_staff", "is_superuser")
 
     def get(self, request, id, format=None):
-        user = get_active_user_or_404(id=id)
-        serializer = WiltUserSerializer(user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        active_user = get_active_user_or_false(id=id)
+        if active_user:
+            serializer = WiltUserSerializer(active_user)
+            response = Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            detail = "Deleted user. (is_active=False)"
+            response = Response(dict(detail=detail), status=status.HTTP_204_NO_CONTENT)
+        return response
 
     def put(self, request, id, format=None):
-        user = get_user_or_404(id=id)
-        fields = self.__filter_fields(request.data)
-        serializer = self.__update(user, fields, partial=False)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        user = get_user_or_false(id=id)
+        if user:
+            response = self.update(user, request.data, partial=False)
+        else:
+            response = self.get_invalid_user(id)
+        return response
 
     def patch(self, request, id, format=None):
-        user = get_user_or_404(id=id)
-        fields = self.__filter_fields(request.data)
-        serializer = self.__update(user, fields, partial=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        user = get_user_or_false(id=id)
+        if user:
+            response = self.update(user, request.data, partial=True)
+        else:
+            response = get_invalid_user_response(id)
+        return response
 
     def delete(self, request, id, format=None):
-        user = get_user_or_404(id=id)
-        serializer = self.__update(user, dict(is_active=False), partial=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        user = get_user_or_false(id=id)
+        if user:
+            serializer = self.__update(user, dict(is_active=False), partial=True)
+            detail = f"User({id}) is deleted."
+            response = Response(dict(detail=detail), status=status.HTTP_204_NO_CONTENT)
+        else:
+            response = get_invalid_user_response(id)
+        return response
+        
+    def update(self, user, data, partial=True):
+        fields = self.__filter_fields(data)
+        serializer = self.__update(user, fields, partial=partial)
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     @staticmethod
     def __update(user, data, partial=False):
@@ -91,6 +97,7 @@ class UserDetail(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return serializer
+
 
     @classmethod
     def __filter_fields(cls, fields):
@@ -151,45 +158,52 @@ class UserCheck(APIView):
 # User가 clap한 Til 목록을 불러오는 view
 class UserClaps(APIView):
     def get(self, request, id, format=None):
-        user = get_active_user_or_404(id=id)
-        queryset = Clap.objects.select_related("til").filter(user=user)
-        user_clap_list = []
-
-        for clap in queryset:
-            til = TilSerializer(clap.til)
-            user_clap_list.append(til.data)
-
-        return Response(user_clap_list, status=status.HTTP_200_OK)
+        active_user = get_active_user_or_false(id=id)
+        if active_user:
+            queryset = Clap.objects.select_related("til").filter(user=active_user)
+            user_clap_list = []
+            for clap in queryset:
+                til = TilSerializer(clap.til)
+                user_clap_list.append(til.data)
+            response = Response(user_clap_list, status=status.HTTP_200_OK)
+        else: 
+            response = get_invalid_user_response(id=id)
+        return response
 
 
 # User가 북마크한 Til 목록을 불러오는 view
 class UserBookmark(APIView):
     def get(self, request, id, format=None):
-        user = get_active_user_or_404(id=id)
-        queryset = Bookmark.objects.select_related("til").filter(user=user)
-        user_bookmark_list = []
+        active_user = get_active_user_or_false(id=id)
+        if active_user:
+            queryset = Bookmark.objects.select_related("til").filter(user=active_user)
+            user_bookmark_list = []
 
-        for bookmark in queryset:
-            til = TilSerializer(bookmark.til)
-            user_bookmark_list.append(til.data)
-
-        return Response(user_bookmark_list, status=status.HTTP_200_OK)
+            for bookmark in queryset:
+                til = TilSerializer(bookmark.til)
+                user_bookmark_list.append(til.data)
+            response =  Response(user_bookmark_list, status=status.HTTP_200_OK)
+        else: 
+            response = get_invalid_user_response(id=id)
+        return response
 
 
 # User가 TIL에 사용하였던 태그들을 불러오는 view
 # User의 태그 중 해당 tag가 들어간 Til 가져오는 view
 class UserTag(APIView):
     def get(self, request, id, format=None):
-        user = get_active_user_or_404(id=id)
-        tag_name = request.GET.get("tag_name", "")
-        tils = Til.objects.filter(user=user).prefetch_related("tags")
 
-        if tag_name:
-            result = self.__til_info_about_tag(tils, tag_name)
-
+        active_user = get_active_user_or_false(id=id)
+        if active_user:
+            tag_name = request.GET.get("tag_name", "")
+            tils = Til.objects.filter(user=active_user).prefetch_related("tags")
+            if tag_name:
+                result = self.__til_info_about_tag(tils, tag_name)
+            else:
+                result = self.__count_tags_in_tils(tils)
+            response = Response(result, status=status.HTTP_200_OK)
         else:
-            result = self.__count_tags_in_tils(tils)
-
+            response = get_invalid_user_response(id=id) 
         return Response(result, status=status.HTTP_200_OK)
 
     @staticmethod
