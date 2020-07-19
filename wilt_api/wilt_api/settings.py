f86f4a64df4639536c07908333f628601a70ab14
Paul Kim <paulracooni@gmail.com>
2020-07-19 22:06:04 +0900

    fix: Delete storages at INSATLLED_APPS

--- wilt_api/wilt_api/settings.py
+++ wilt_api/wilt_api/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Third Party
-    "storages",
+    # "storages",
     # Local
     "wilt_backend",
     # "firebase_authentication",
