b52f8994ab180810b417128c0ce6ca521e3c14a3
Paul Kim <paulracooni@gmail.com>
2020-08-09 21:39:21 +0900

    fix: Fix for no Til, WiltUser at Admin page

--- wilt_api/wilt_backend/admin.py
+++ wilt_api/wilt_backend/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from wilt_backend.models import *
 
 # Register your models here.
-admin.site.register(Til)
-admin.site.register(WiltUser)
+# admin.site.register(Til)
+# admin.site.register(WiltUser)
